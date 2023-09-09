6e2d12ba0e77ba080fd9c895325018bf335a547f
Veronica Garcia <veroagarcia90@gmail.com>
2023-09-09 14:27:19 -0300

    creacion de entorno, React.Component, useState, useEffect y Métodos del ciclo de vida en React.Component

---
+++ manejo-de-estados/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { UseState } from './UseState.js';
+import { ClassState } from './ClassState.js';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <UseState name= "UseState"/>
+      <ClassState name="ClassState" />
+    </div>
+  );
+}
+
+export default App;
---
+++ manejo-de-estados/src/UseState.js
@@ -0,0 +1,43 @@
+import React from "react";
+
+function UseState({ name }) {
+    const [error, setError] = React.useState(true);
+    const [loading, setLoading] = React.useState(false);
+  
+    React.useEffect(() => {
+      console.log("Empezando el efecto")
+  
+        if (loading) {
+            setTimeout(() => {
+                console.log("Haciendo la validación")
+        
+                setLoading(false);
+        
+                console.log("terminando la validación")
+            }, 3000);
+        }
+  
+      console.log("Terminando el efecto")
+    }, [loading]);
+  
+    return (
+      <div>
+        <h2>Eliminar {name}</h2>
+        
+        <p>Por favor, escribe el código de seguridad.</p>
+        {error && (
+          <p>Error: el código es incorrecto</p>
+        )}
+        {loading && (
+          <p>Cargando...</p>
+        )}
+  
+        <input placeholder="Código de seguridad" />
+        <button
+          onClick={() => setLoading(true)}
+        >Comprobar</button>
+      </div>
+    );
+}
+
+export {UseState};
