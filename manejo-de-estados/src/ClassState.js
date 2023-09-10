0489495caee06964b0cb1827160bdee4e65dabab
Veronica Garcia <veroagarcia90@gmail.com>
2023-09-10 10:49:39 -0300

    validacion de codigo mediante estados compuestos

--- manejo-de-estados/src/ClassState.js
+++ manejo-de-estados/src/ClassState.js
@@ -1,25 +1,31 @@
 import React from "react";
 import { Loading } from './Loading';
 
+const SECURITY_PASS = 'paradigma'; 
+
 class ClassState extends React.Component {
     constructor(props) {
         super(props);
 
         this.state = {
-            error: true,
+            error: false,
             loading: false,
+            value: '',
         };
     }
     componentDidUpdate() {
-        console.log('actualizacion');
 
         if (this.state.loading) {
             setTimeout(() => {
-                console.log("Haciendo la validación")
 
-                this.setState({ loading: false });
+                if(this.state.value === SECURITY_PASS) {
+                    
+                    this.setState({ loading: false, error: false });
+                } else {
+                    this.setState({ loading: false, error: true });
+                }
 
-                console.log("terminando la validación")
+
             }, 3000);
         }
     }
@@ -30,7 +36,8 @@ class ClassState extends React.Component {
                 <h2>Eliminar {this.props.name}</h2>
             
                 <p>Por favor, escribe el código de seguridad.</p>
-                {this.state.error && (
+
+                {(this.state.error && !this.state.loading) && (
                     <p>Error: el código es incorrecto</p>
                 )}
 
@@ -38,7 +45,10 @@ class ClassState extends React.Component {
                     <Loading />
                 )}
 
-                <input placeholder="Código de seguridad" />
+                <input placeholder="Código de seguridad" 
+                    value={this.state.value}
+                    onChange={(event) => {this.setState({ value: event.target.value })}}
+                />
                 <button
                     onClick={() => this.setState({ loading: true })}
                 >Comprobar</button>
--- manejo-de-estados/src/UseState.js
+++ manejo-de-estados/src/UseState.js
@@ -1,43 +1,62 @@
 import React from "react";
 
+const SECURITY_PASS = 'paradigma'; 
+
 function UseState({ name }) {
-    const [error, setError] = React.useState(true);
-    const [loading, setLoading] = React.useState(false);
+
+  const [state, setState] = React.useState({
+    error: false,
+    loading: false,
+    value: ''
+  })
+  React.useEffect(() => {
+    if (state.loading) {
+      setTimeout(() => {
+
+        if (state.value === SECURITY_PASS) {
+          setState({
+            ...state,
+            error:false,
+            loading:false
+          });
+        } else {
+          setState({
+            ...state,
+            error:true,
+            loading: false
+          });
+        } 
+      }, 3000);
+    }
+  }, [state.loading]);
   
-    React.useEffect(() => {
-      console.log("Empezando el efecto")
+  return (
+    <div>
+      <h2>Eliminar {name}</h2> 
+      <p>Por favor, escribe el código de seguridad.</p>
+
+      {(state.error && !state.loading) && (
+        <p>Error: el código es incorrecto</p>
+      )}
+
+      {state.loading && (
+        <p>Cargando...</p>
+      )}
   
-        if (loading) {
-            setTimeout(() => {
-                console.log("Haciendo la validación")
-        
-                setLoading(false);
-        
-                console.log("terminando la validación")
-            }, 3000);
-        }
-  
-      console.log("Terminando el efecto")
-    }, [loading]);
-  
-    return (
-      <div>
-        <h2>Eliminar {name}</h2>
-        
-        <p>Por favor, escribe el código de seguridad.</p>
-        {error && (
-          <p>Error: el código es incorrecto</p>
-        )}
-        {loading && (
-          <p>Cargando...</p>
-        )}
-  
-        <input placeholder="Código de seguridad" />
-        <button
-          onClick={() => setLoading(true)}
-        >Comprobar</button>
-      </div>
-    );
+      <input placeholder="Código de seguridad"
+        value={state.value}
+        onChange= {(event)=> {setState({
+          ...state,
+          value: event.target.value})}} 
+      />
+      <button
+        onClick={() => setState({
+          ...state,
+          loading: true})}>
+        Comprobar
+      </button>
+    </div>
+  );
 }
 
 export {UseState};
